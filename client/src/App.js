502816612997d7f39247d680b8e9cf5481416701
KHomburg <kevin.homburg@outlook.de>
2019-11-22 12:24:22 +0100

    basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,20 @@ import React, {Fragment, useEffect} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom' 
 import './styles/App.scss';
 
+//Layout
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Header from './components/layout/Header'
+
+
+//PAGES
 import Landing from './components/layout/Landing'
 import Register from './components/pages/auth/Register'
 import Login from './components/pages/auth/Login'
 import Alert from './components/layout/Alert'
 import User from './components/pages/users/User'
 import UserIndex from './components/pages/users/UserIndex'
-
 import Note from './components/pages/notes/Note'
 import CreateNote from './components/pages/notes/CreateNote'
 import NoteIndex from './components/pages/notes/NoteIndex'
@@ -35,20 +41,28 @@ const App = () => {
       <Router>
         <Fragment>
           <Header />
-          <Route exact path="/" component={Landing} />
-          <section className="containter">
-            <Alert />
-            <Switch>
-              <Route exact path="/users/register" component={Register} />
-              <Route exact path="/users/login" component={Login} />
-              <PrivateRoute exact path="/users" component={UserIndex} />
-              <PrivateRoute exact path="/users/:id" component={User} />
-              <PrivateRoute exact path="/notes/new" component={CreateNote} />
-              <PrivateRoute exact path="/notes/:id" component={Note} />
-              <PrivateRoute exact path="/notes" component={NoteIndex} />
-              <PrivateRoute exact path="/test" component={Landing} />
-            </Switch>
-          </section>
+          <Container>
+            <Row>
+              <Col>
+              <Route exact path="/" component={Landing} />
+              <section className="containter">
+                <Alert />
+                <Switch>
+                  <Route exact path="/users/register" component={Register} />
+                  <Route exact path="/users/login" component={Login} />
+                  <PrivateRoute exact path="/users" component={UserIndex} />
+                  <PrivateRoute exact path="/users/:id" component={User} />
+                  <PrivateRoute exact path="/notes/new" component={CreateNote} />
+                  <PrivateRoute exact path="/notes/:id" component={Note} />
+                  <PrivateRoute exact path="/notes" component={NoteIndex} />
+                  <PrivateRoute exact path="/test" component={Landing} />
+                </Switch>
+              </section>
+              </Col>
+            </Row>
+          </Container>
+          
+
         </Fragment>
       </Router>
     </Provider>
